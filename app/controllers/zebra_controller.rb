de75334c96214e085934c01f3d8140e70427f108
yzemlyak <yzemlyak@gmail.com>
2025-04-27 03:35:25 +0000

    Some progress

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,39 +1,17 @@
 class ZebraController < ApplicationController
-  def giraffe 
-    @first_roll = rand(1..6)
-    @second_roll = rand(1..6)
-    end
-    
+  def two_six 
     render({ :template => "game_templates/two_six" })
   end
-
-  def lion 
-    @first_roll = rand(1..10)
-    @second_roll = rand(1..10)
-    end
-    
+  def two_ten 
     render({ :template => "game_templates/two_ten" })
   end
-
-  def tiger 
-    @first_roll = rand(1..20)
-  end
-    
+  def one_twenty 
     render({ :template => "game_templates/one_twenty" })
   end
-
-  def jaguar
-    @first_roll = rand(1..4)
-    @second_roll = rand(1..4)
-    @third_roll = rand(1..4)
-    @fourth_roll = rand(1..4)
-    @fifth_roll = rand(1..4)
-    end
-    
+  def five_four
     render({ :template => "game_templates/five_four" })
   end
-
-  def home
-    render({ :template => "game_templates/home" })
+  def homepage
+    render({ :template => "game_templates/homepage" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "zebra",:action => "home" })
-  get("/dice/2/6", { :controller => "zebra",:action => "giraffe" })
-  get("/dice/2/10", { :controller => "zebra",:action => "lion" })
-  get("/dice/1/20", { :controller => "zebra",:action => "tiger" })
-  get("/dice/5/4", { :controller => "zebra",:action => "jaguar" })
+  get("/", { :controller => "zebra",:action => "homepage" })
+  get("/dice/2/6", { :controller => "zebra",:action => "two_six" })
+  get("/dice/2/10", { :controller => "zebra",:action => "two_ten" })
+  get("/dice/1/20", { :controller => "zebra",:action => "one_twenty" })
+  get("/dice/5/4", { :controller => "zebra",:action => "five_four" })
 end
