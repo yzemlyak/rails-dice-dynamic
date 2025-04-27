1b1ebe8b441eb03a5416483880c2ab2e8216ac5d
yzemlyak <yzemlyak@gmail.com>
2025-04-27 03:55:14 +0000

    83% done

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "zebra",:action => "homepage" })
-  #get("/dice/:first_spot/:second_spot") do
-    #@num_dice = params.fetch("first_spot").to_i
-    #@num_sides = params.fetch("second_spot")
-    #@rolls = Array.new
-    #@num_dice.times.do
-    #  die = rand(1..@num_sides)
-   #   @rolls.push(die)
-  #end
   get("/dice/2/6", { :controller => "zebra",:action => "two_six" })
   get("/dice/2/10", { :controller => "zebra",:action => "two_ten" })
   get("/dice/1/20", { :controller => "zebra",:action => "one_twenty" })
